4271bca936a0be51c5bb5d68642703f6a722f21f
Yoram Erez <yoramerez@gmail.com>
2025-06-08 22:16:41 +0300

    מחיקת App.css לאחר מעבר ל-index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-   /** @type {import('tailwindcss').Config} */
-   module.exports = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: [
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
@@ -7,4 +7,5 @@
       extend: {},
     },
     plugins: [],
-  }
+  };
+  
